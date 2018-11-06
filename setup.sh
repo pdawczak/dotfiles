e06346fc204f64d6866a22ba4a9ea05d8166bbd8
Paweł Dawczak <pawel.dawczak@gmail.com>
2018-11-06 07:43:20 +0000

    Update setup script to install HTTPie and Emacs

--- setup.sh
+++ setup.sh
@@ -4,3 +4,10 @@ brew tap Goles/battery
 brew install battery
 
 brew install reattach-to-user-namespace
+
+brew install httpie
+
+# https://github.com/railwaycat/homebrew-emacsmacport
+brew tap railwaycat/emacsmacport
+# brew install emacs-mac
+brew cask install emacs-mac
